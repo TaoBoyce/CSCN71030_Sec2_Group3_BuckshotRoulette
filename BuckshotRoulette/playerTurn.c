cdf597f8af0323be8031338a3a752a199fe17e6b
Tao Boyce <tao.z.boyce@gmail.com>
2024-04-03 14:16:30 -0400

    playerTurn()

--- BuckshotRoulette/playerTurn.c
+++ BuckshotRoulette/playerTurn.c
@@ -17,22 +17,74 @@ bool playerTurn(int lives[], BulletsLink* bullets, ITEM* items[], bool* oppHandc
 	//show usable items
 	displayPlayerChoices(items[PLAYER]);
 	//select choice using number from player
-	int choice = getIntInput("Input a number: ");
-	//if it's an item continue looping unless there's no usable items left
+	int choice;
+	bool doubleDamage = false;
+	bool bulletKnown = false;
+	while (choice != 1) {
+		choice = getIntInput("Input a number: ");
+		//if it's an item continue looping unless there's no usable items left
+		if (choice <= 0) {
+			fprintf(stdout, "Invalid input, try again.\n");
+			continue;
+		}
+		if (choice == 1) {//aim gun was chosen
+			break;
+		}
+		if (choice == 2) {
+			//save game
+			continue;
+		}
+		if (choice == 3) {
+			//load game
+			continue;
+		}
+		int j = 3;
+		for (int i = 0; i < ITEMS_CAP; i++) {
+			ITEM* pItem = &(items[PLAYER][i]);
+			if (pItem == EMPTY) {
+				continue;
+			}
+			j++;
+			if (j == choice) {
+				useItem(pItem, bullets, oppHandcuffed, lives[PLAYER], &doubleDamage, &bulletKnown);
+				break;
+			}
+		}
+	}
+	
 	//if it's the gun, ask player who they're going to shoot
+	fprintf(stdout, "Select an option: \n");
+	fprintf(stdout, "1: Shoot the Dealer.\n");
+	fprintf(stdout, "2: Shoot yourself. (Shooting yourself with a blank skips the opponent's turn.)\n");
+	int target;
+	while (target != 1 | target != 2) {
+		target = getIntInput("Input a number: ");
+		if (target != 1 | target != 2) {
+			fprintf(stdout, "Invalid input, try again.\n");
+		}
+	}
+
+	if (target == 1) {//shoot dealer
+		return shootGun(lives, bullets, false, doubleDamage);
+	}
+	if (target == 2) {//shoot self
+		return shootGun(lives, bullets, true, doubleDamage);
+	}
+	return false;
 }
 
 void displayPlayerChoices(ITEM pItems[]) {
 	fprintf(stdout, "Select an option: \n");
-	fprintf(stdout, "1: Aim GUN.\n");
+	fprintf(stdout, "1: Aim GUN. (Ends the turn.)\n");
 	fprintf(stdout, "2: Save Game.\n");
 	fprintf(stdout, "3: Load Game.\n");
 	int j = 3;
 	for (int i = 0; i < ITEMS_CAP; i++) {
-		if (pItems[i] != EMPTY) {
-			j++;
-			fprintf(stdout, "%d: Use item - %s\n", j, item_text(pItems[i]));
+		if (pItems[i] == EMPTY) {
+			continue;
 		}
+		j++;
+		fprintf(stdout, "%d: Use item - %s\n", j, item_text(pItems[i]));
 	}
 }
 
