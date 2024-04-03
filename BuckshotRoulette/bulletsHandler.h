817b955c269ba5644b78c0ad3273fbb05eacbe76
Tao Boyce <tao.z.boyce@gmail.com>
2024-04-03 13:05:29 -0400

    Revert "getting bullet.h"
    This reverts commit b8d0d4a3cd020be095fa8e969b03f87672ba58ab.

--- BuckshotRoulette/bulletsHandler.h
+++ BuckshotRoulette/bulletsHandler.h
@@ -8,21 +8,8 @@ typedef enum bullet {
 typedef struct bulletsNode {
 	BULLET bullet;
 	struct bulletsNode* next;
-}BulletsNode, * BulletLink;
+}BulletsNode, *BulletsLink;
 
-void loadRandomBullets(BulletLink* head, int numBullets);
+BULLET popBullet(BulletsLink* link);
 
-void clearBullets(BulletLink* head);
-
-void addBullet(BulletLink* head, bullet type);
-
-void printBulletsInOrder(BulletLink head);
-
-bullet peekBullet(BulletLink head);
-
-bullet popBullet(BulletLink* head);
-
-bullet fireBullet(BulletLink* head);
-
-void printBulletCounts(BulletLink head);
-
+BULLET peekBullet(BulletsLink link);
