516060f8d26dd4ff4cda2c0041f13784c756b645
Tao Boyce <tao.z.boyce@gmail.com>
2024-04-05 15:08:32 -0400

    main game manager basis

--- BuckshotRoulette/Main.c
+++ BuckshotRoulette/Main.c
@@ -4,6 +4,17 @@
 #include"textInput.h"
 
 int main(void) {
+	//get console input
+	//convert console input to infinite mode and difficulty
+	//ask for name
+	//start round
+	//load bullets and items
+	//loop player's turn until they don't skip opponent/run out of bullets
+	//loop dealer's turn until they don't skip opponent/run out of bullets
+	//loop both of the above until one of their lives hits 0
+	//3 rounds in non-infinite
+	//infinite rounds in infinite
 
+	//clean up 
 	return 0;
 }
